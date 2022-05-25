7d93fd632e1a320a65e0a510066fca6b688f2eaf
Areeb Ali <areeb.algo@outlook.com>
2022-05-26 03:19:35 +0500

    Update to hangman project

--- BeginnerProjects Python/6.Hangman/main.py
+++ BeginnerProjects Python/6.Hangman/main.py
@@ -1,15 +1,26 @@
-from asyncio import selector_events
+
 import random
-from select import select
 
 word_list = ["aardvark", "baboon", "camel"]
-
-
 selected_word= random.choice(word_list)
 print(selected_word)
-guess= input("Please guess the word: ").lower()
 
-if(selected_word==guess):
-    print("You're right")
-else:
-    print("You're wrong")
+display=[]
+### This is to generate a display with empty places with the same size of the word that is to be guessed ###
+for letters in selected_word:
+    display+="_"
+print(display)
+
+tries=5
+
+guess=""
+
+for j in range(len(selected_word)):
+    guess= input("Please guess the letter: ").lower()
+    for i in range(len(selected_word)):
+        if(selected_word[i]==guess[0]):
+            print(selected_word[i])
+            display[i]=selected_word[i]
+        else:
+            tries-=1
+    print(display)
