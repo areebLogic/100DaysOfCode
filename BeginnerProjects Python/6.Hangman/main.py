539989b861b91d7d699eb97813f36b536104c277
Areeb Ali <areeb.algo@outlook.com>
2022-05-27 00:44:54 +0500

    Tested version of the hangman game: update

--- BeginnerProjects Python/6.Hangman/main.py
+++ BeginnerProjects Python/6.Hangman/main.py
@@ -1,8 +1,66 @@
 
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 selected_word= random.choice(word_list)
+guess=""
 print(selected_word)
 
 display=[]
@@ -11,16 +69,33 @@ for letters in selected_word:
     display+="_"
 print(display)
 
-tries=5
+tries=6
 
-guess=""
 
-for j in range(len(selected_word)):
-    guess= input("Please guess the letter: ").lower()
+# A nested loop is being used, the outer loop will run till whe word is successfully completed or
+# until the player has exhausted the tries.
+game_end= False
+
+
+while not game_end:
+    
+    guess= input("Please guess the next letter: ").lower()
     for i in range(len(selected_word)):
-        if(selected_word[i]==guess[0]):
-            print(selected_word[i])
-            display[i]=selected_word[i]
-        else:
+        
+        if guess not in selected_word:
             tries-=1
+            if tries>0:
+                print(stages[tries])
+                print("Wrong letter, try again")
+                break
+            else:
+                print(stages[0])
+                print("You've lost the game")
+                game_end= True
+                break
+        elif(selected_word[i]==guess[0]):
+            display[i]=selected_word[i]
+            if "_" not in display:
+                game_end= True
+                print("You won the game!!")
     print(display)
